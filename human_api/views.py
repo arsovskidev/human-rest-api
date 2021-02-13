d613c4ab2523441ec2b016851ec2983f541d4f35
Филип Арсовски <arshetamine@protonmail.com>
2021-02-13 19:07:52 +0100

    Added searching for human profile API

--- human_api/views.py
+++ human_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from human_api import serializers
 from human_api import models
@@ -102,7 +103,10 @@ class HumanViewSet(viewsets.ViewSet):
 
 class HumanProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating human profiles"""
+
     serializer_class = serializers.HumanProfileSerializer
     queryset = models.HumanProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
