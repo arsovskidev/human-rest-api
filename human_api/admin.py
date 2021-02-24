adee9f8a57639e89b3aa121f497d5af517bd6d9b
Филип Арсовски <arshetamine@protonmail.com>
2021-02-24 16:19:22 +0100

    Cleaning master branch.

--- human_api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from human_api import models
-
-
-admin.site.register(models.HumanProfile)
-admin.site.register(models.HumanCaseItem)
--- human_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HumanApiConfig(AppConfig):
-    name = 'human_api'
